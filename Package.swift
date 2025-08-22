6379d0bb968b2236cdf2a7bb0da0c7dc8b5c8ab5
phoenix <x.rhythm@qq.com>
2025-08-22 22:50:50 +0800

    Delete WebP

--- Package.swift
+++ Package.swift
@@ -15,10 +15,9 @@ let package = Package(
     targets: [
         .target(
             name: "YYImage",
-            path: "YYImage",
             publicHeadersPath: "",
             cSettings: [
-                .define("NS_BLOCK_ASSERTIONS", to: "1", .when(configuration: .release)),
+                .define("NS_BLOCK_ASSERTIONS", to: "1", .when(configuration: .release))
             ],
             linkerSettings: [
                 .linkedLibrary("z"),
@@ -34,58 +33,4 @@ let package = Package(
     ]
 )
 
-/*
- let package = Package(
-     name: "YYImage",
-     platforms: [
-         .iOS(.v12)
-     ],
-     products: [
-         .library(
-             name: "YYImage",
-             targets: ["YYImage"]
-         ),
-         .library(
-             name: "YYImage_libwebp",
-             targets: ["YYImage", "YYImage_libwebp"]
-         )
-     ],
-     dependencies: [
-         .package(url: "https://github.com/SDWebImage/libwebp-Xcode", from: "1.5.0")
-     ],
-     targets: [
-         .target(
-             name: "YYImage",
-             path: "YYImage",
-             publicHeadersPath: "",
-             cSettings: [
-                 .define("NS_BLOCK_ASSERTIONS", to: "1", .when(configuration: .release)),
-                 .define("YYIMAGE_WEBP_ENABLED", to: "1")
-             ],
-             linkerSettings: [
-                 .linkedLibrary("z"),
-                 .linkedFramework("UIKit"),
-                 .linkedFramework("CoreFoundation"),
-                 .linkedFramework("QuartzCore"),
-                 .linkedFramework("AssetsLibrary"),
-                 .linkedFramework("ImageIO"),
-                 .linkedFramework("Accelerate"),
-                 .linkedFramework("MobileCoreServices")
-             ]
-         ),
-         .target(
-             name: "YYImage_libwebp",
-             dependencies: [
-                 "YYImage",
-                 .product(name: "libwebp", package: "libwebp-Xcode")
-             ],
-             path: "YYImage_libwebp",
-             cSettings: [
-                 .define("NS_BLOCK_ASSERTIONS", to: "1", .when(configuration: .release)),
-                 .define("YYIMAGE_WEBP_ENABLED", to: "1")
-             ]
-         )
-     ]
- )
 
-*/
